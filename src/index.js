9fbdfd1b3a327db6022d4f7233cf1eacf4fca79c
unknown <cbelden@cbelden.adage.adagetechnologies.com>
2015-05-15 15:51:51 -0500

    Add board styling/templating. Create function to generate game state information.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 const $ = require('jquery');
+const gameBoardTemplate = require('../partials/board.handlebars');
+
+const DefaultGameState = [
+  ['x', '', ''],
+  ['', 'x', ''],
+  ['o', '', '']
+];
+
+$('.game-board').html(gameBoardTemplate({ rows: gameInformation(DefaultGameState)}));
+
+
+/**
+ * Returns a modified gameState with some positional information
+ */
+function gameInformation(gameState) {
+  return DefaultGameState
+    .map(function (row, rowIndex) {
+      return row
+        .map(function (item, itemIndex) {
+          console.log(item);
+          return {
+            position: [rowIndex, itemIndex],
+            status: item
+          }
+        });
+    });
+}
